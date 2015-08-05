2f3266827baf46a7f3204dbd76652b62454b885c
Wessam Abdrabo <wessam.abdrabo@tum.de>
2015-08-05 21:33:08 +0200

    adding pan gesture recgnizer and playing video on server on event

--- recSysAppV2/DetailsViewController.m
+++ recSysAppV2/DetailsViewController.m
@@ -18,6 +18,8 @@
     MPMoviePlayerViewController *_moviePlayerController;
     VideoCast* _video;
 }
+@property(nonatomic) CGAffineTransform transform;   // default is CGAffineTransformIdentity. animatable
+@property(strong,nonatomic)    UIDynamicAnimator* animator;
 @end
 
 @implementation DetailsViewController
@@ -34,6 +36,11 @@
     [NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:imageUrl]] queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
         self.imageView.image = [UIImage imageWithData:data];
     }];
+    
+    UIGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
+    [self.imageView addGestureRecognizer:pan];
+    
+    self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -55,8 +62,6 @@
 #pragma mark - Video player
 
 - (IBAction)watchVideoClicked:(id)sender {
-    [[CommManager sharedManager] sendMessage:_video.title];
-    
 //    NSURL *url = [NSURL URLWithString:_video.url];
 //    
 //    
@@ -91,6 +96,115 @@
     
     //Show rating
     [self performSegueWithIdentifier:@"showRating" sender:self];
+}
+
+#pragma mark - pan gesture
+- (void)handlePan:(UIPanGestureRecognizer *)gesture {
+    static UIAttachmentBehavior *attachment;
+    static CGPoint               startCenter;
+    
+    // variables for calculating angular velocity
+    
+    static CFAbsoluteTime        lastTime;
+    static CGFloat               lastAngle;
+    static CGFloat               angularVelocity;
+    
+    if (gesture.state == UIGestureRecognizerStateBegan) {
+        
+        [[CommManager sharedManager] sendMessage:_video.title];
+        
+        [self.animator removeAllBehaviors];
+        
+        startCenter = gesture.view.center;
+        
+        // calculate the center offset and anchor point
+        
+        CGPoint pointWithinAnimatedView = [gesture locationInView:gesture.view];
+        
+        UIOffset offset = UIOffsetMake(pointWithinAnimatedView.x - gesture.view.bounds.size.width / 2.0,
+                                       pointWithinAnimatedView.y - gesture.view.bounds.size.height / 2.0);
+        
+        CGPoint anchor = [gesture locationInView:gesture.view.superview];
+        
+        // create attachment behavior
+        
+        attachment = [[UIAttachmentBehavior alloc] initWithItem:gesture.view
+                                               offsetFromCenter:offset
+                                               attachedToAnchor:anchor];
+        
+        // code to calculate angular velocity (seems curious that I have to calculate this myself, but I can if I have to)
+        
+        lastTime = CFAbsoluteTimeGetCurrent();
+        lastAngle = [self angleOfView:gesture.view];
+        
+        typeof(self) __weak weakSelf = self;
+        
+        attachment.action = ^{
+            CFAbsoluteTime time = CFAbsoluteTimeGetCurrent();
+            CGFloat angle = [weakSelf angleOfView:gesture.view];
+            if (time > lastTime) {
+                angularVelocity = (angle - lastAngle) / (time - lastTime);
+                lastTime = time;
+                lastAngle = angle;
+            }
+        };
+        
+        // add attachment behavior
+        
+        [self.animator addBehavior:attachment];
+    } else if (gesture.state == UIGestureRecognizerStateChanged) {
+        // as user makes gesture, update attachment behavior's anchor point, achieving drag 'n' rotate
+        
+        CGPoint anchor = [gesture locationInView:gesture.view.superview];
+        attachment.anchorPoint = anchor;
+    } else if (gesture.state == UIGestureRecognizerStateEnded) {
+        [self.animator removeAllBehaviors];
+        
+        CGPoint velocity = [gesture velocityInView:gesture.view.superview];
+        
+        // if we aren't dragging it down, just snap it back and quit
+        
+        if (fabs(atan2(velocity.y, velocity.x) - M_PI_2) > M_PI_4) {
+            UISnapBehavior *snap = [[UISnapBehavior alloc] initWithItem:gesture.view snapToPoint:startCenter];
+            [self.animator addBehavior:snap];
+            
+            return;
+        }
+        
+        // otherwise, create UIDynamicItemBehavior that carries on animation from where the gesture left off (notably linear and angular velocity)
+        
+        UIDynamicItemBehavior *dynamic = [[UIDynamicItemBehavior alloc] initWithItems:@[gesture.view]];
+        [dynamic addLinearVelocity:velocity forItem:gesture.view];
+        [dynamic addAngularVelocity:angularVelocity forItem:gesture.view];
+        [dynamic setAngularResistance:1.25];
+        
+        // when the view no longer intersects with its superview, go ahead and remove it
+        
+        typeof(self) __weak weakSelf = self;
+        
+        dynamic.action = ^{
+            if (!CGRectIntersectsRect(gesture.view.superview.bounds, gesture.view.frame)) {
+                
+                [weakSelf.animator removeAllBehaviors];
+                [gesture.view removeFromSuperview];
+                
+                [[[UIAlertView alloc] initWithTitle:nil message:@"Your video will now start on other display" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+            }
+        };
+        [self.animator addBehavior:dynamic];
+        
+        // add a little gravity so it accelerates off the screen (in case user gesture was slow)
+        
+        UIGravityBehavior *gravity = [[UIGravityBehavior alloc] initWithItems:@[gesture.view]];
+        gravity.magnitude = 0.7;
+        [self.animator addBehavior:gravity];
+    }
+}
+
+- (CGFloat)angleOfView:(UIView *)view{
+    // http://stackoverflow.com/a/2051861/1271826
+    
+    return atan2(view.transform.b, view.transform.a);
 }
 
 @end
